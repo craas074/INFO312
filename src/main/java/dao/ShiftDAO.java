cc0a9ca86e00894b58e6a619b29a3c478f611577
Cameron Miller <cameron.william.miller@gmail.com>
2015-07-27 14:23:18 +1200

    update shiftDAO to an ArrayList (does not need to be a map)

--- src/main/java/dao/ShiftDAO.java
+++ src/main/java/dao/ShiftDAO.java
@@ -6,7 +6,9 @@
 package dao;
 
 import domain.Shift;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,34 +18,19 @@ import java.util.Map;
 public class ShiftDAO {
     
     
-    Map<String, Shift> shifts = new HashMap<>();
+    private List<Shift> shifts = new ArrayList<>();
 
-    public Map<String, Shift> getShifts() {
+    public List<Shift> getShifts() {
         return shifts;
     }
-
-    public void setShifts(Map<String, Shift> shifts) {
-        this.shifts = shifts;
-    }
-
-   
-
-    public void addShift(Shift s){
-        shifts.put(s.getId(), s);
+    
+    public void add(Shift s){
+        if(shifts.contains(s)){
+            throw new IllegalArgumentException("Shift already exists");
+        }
+        shifts.add(s);
     }
     
-    public void deleteShift(Shift s){
-        shifts.remove(s.getId());
-    }
-    
-    public void updateShift(Shift s){
-        //update??
-    }
-    
-    
-    public Shift getShift(String id){
-        return shifts.get(id);
-    }
     
     //not sure this method is right
     
