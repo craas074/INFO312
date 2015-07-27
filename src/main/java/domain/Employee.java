fd5e374b9d98b475e49b96f3b5fdb3f3745fb9d5
Cameron Miller <cameron.william.miller@gmail.com>
2015-07-27 13:31:11 +1200

    dao init

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -3,12 +3,104 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package domain;
+package Domain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author cameron
+ * @author benjamindawson-bruce
  */
 public class Employee {
+    
+    
+     String id;
+     String name;
+     Double minhours;
+     Double maxhours;
+     Double currenthours;
+     
+     
+     List<Shift> availableShifts = new ArrayList<>();
+     
+     List<Shift> completedShifts = new ArrayList<>();
+
+    public Employee(String id, String name, Double minhours, Double maxhours, Double currenthours) {
+        this.id = id;
+        this.name = name;
+        this.minhours = minhours;
+        this.maxhours = maxhours;
+        this.currenthours = currenthours;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMinhours() {
+        return minhours;
+    }
+
+    public void setMinhours(Double minhours) {
+        this.minhours = minhours;
+    }
+
+    public Double getMaxhours() {
+        return maxhours;
+    }
+
+    public void setMaxhours(Double maxhours) {
+        this.maxhours = maxhours;
+    }
+
+    public Double getCurrenthours() {
+        return currenthours;
+    }
+
+    public void setCurrenthours(Double currenthours) {
+        this.currenthours = currenthours;
+    }
+
+    public List<Shift> getAvailableShifts() {
+        return availableShifts;
+    }
+
+    public void setAvailableShifts(List<Shift> availableShifts) {
+        this.availableShifts = availableShifts;
+    }
+
+    public List<Shift> getCompletedShifts() {
+        return completedShifts;
+    }
+
+    public void setCompletedShifts(List<Shift> completedShifts) {
+        this.completedShifts = completedShifts;
+    }
+     
+     
+    
+    public void addAvailableShift(Shift shift){
+        
+        availableShifts.add(shift);
+    }
+    
+    public void addCompletedShift(Shift shift){
+        
+        completedShifts.add(shift);
+    }
+     
     
 }
--- src/main/java/domain/Shift.java
+++ src/main/java/domain/Shift.java
@@ -3,12 +3,93 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package domain;
+package Domain;
+
+
+
+import java.util.Date;
 
 /**
  *
- * @author cameron
+ * @author benjamindawson-bruce
  */
 public class Shift {
     
+    
+   String id;
+   Integer start;
+   Integer end;
+   //made start and end integers, easier to work with in 24 hr time
+   Date date;
+   Employee assignedTo;
+
+    public Shift(String id, Integer start, Integer end, Date date, Employee assignedTo) {
+        this.id = id;
+        this.start = start;
+        this.end = end;
+        this.date = date;
+        this.assignedTo = assignedTo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getEnd() {
+        return end;
+    }
+
+    public void setEnd(Integer end) {
+        this.end = end;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Employee getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(Employee assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+    
+    
+    
+    
+    public void Assign (Employee employee){
+    
+        //wasnt sure how to implement this method
+    }
+
+            
+    public Integer getShiftLength(){
+        
+        
+        return end-start;
+    
+    }
+   
+   
+   
+    
 }
+
+
