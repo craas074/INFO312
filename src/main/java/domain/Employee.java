173b8b97a3a7c0236f1011d65a189e2fa2e1d731
Cameron Miller <cameron.william.miller@gmail.com>
2015-07-31 23:47:58 +1200

    setup the constructor for Employee so that it works in the application state.
    passwords will be hashed via SHA256
    
    email is sent to employee with their initial password. The firstLogin boolean should be set to true so that the application knows to prompt them to change their password at login. It will also be possible to recover passwords.

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -5,8 +5,22 @@
  */
 package domain;
 
+
+import com.google.appengine.repackaged.org.apache.commons.codec.digest.DigestUtils;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  *
@@ -16,22 +30,89 @@ public class Employee {
     
     
      private String id;
+     private String email;
+     private String password;
      private String name;
      private Double minhours;
      private Double maxhours;
      private Double currenthours;
-     
+     private Boolean firstLogin;
+
      
      List<Shift> availableShifts = new ArrayList<>();
      
      List<Shift> completedShifts = new ArrayList<>();
 
-    public Employee(String id, String name, Double minhours, Double maxhours, Double currenthours) {
-        this.id = id;
+    public Employee(String name, String email, Double minhours, Double maxhours) {
+        this.id = UUID.randomUUID().toString();
+        this.email = email;
         this.name = name;
-        this.minhours = minhours;
+        this.minhours=minhours;
         this.maxhours = maxhours;
-        this.currenthours = currenthours;
+        this.password = newPassword(email);
+        this.currenthours = 0.00;
+    }
+    
+    /* email generated pass to the employee, hash that pass and store the employee in the DAO
+    we want to avoid this being accessed, so keep it private*/
+    private String newPassword(String email){
+        if(email.equals("")){
+            throw new IllegalArgumentException("email must not be null");
+        }
+        String pass = RandomStringUtils.random(8);
+        //email the employee the password they can use to login
+        
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        
+        String messagebody = String.format("Dear %s %n"
+                + "%n"
+                + "Your account is now ready to login and submit availibility at URL %n"
+                + "%n"
+                + "login: %s %n"
+                + "password: %s %n"
+                + "%n"
+                + "Regards,"
+                + "Administration",getName(), getEmail(), RandomStringUtils.random(8));
+        try{
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("noreply.unipol.admin@gmail.com", "Administration"));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(getEmail(), getName()));
+            msg.setSubject("Your account has been activated");
+            msg.setText(messagebody);
+            Transport.send(msg);
+        }
+        catch 
+                (MessagingException | UnsupportedEncodingException ex) {
+             Logger.getLogger(Employee.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        //hash the string and set the employee's password to the hashed one. USED SHA256
+        String hash = DigestUtils.sha256Hex(pass);
+        return hash;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Boolean getFirstLogin() {
+        return firstLogin;
+    }
+
+    public void setFirstLogin(Boolean firstLogin) {
+        this.firstLogin = firstLogin;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getId() {
